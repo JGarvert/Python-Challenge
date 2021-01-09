c4cd2dfbb817799a705703f4ade3d2a63852793a
JGarvert <julie.garvert@adidas.com>
2021-01-09 09:36:19 -0800

    Modify and cleaning of part 1

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,22 +10,15 @@ bud_data_path = os.path.join("","Resources","budget_data.csv")
 with open(bud_data_path, 'r', newline="") as bud_data:
     csv_reader = csv.reader(bud_data, delimiter=",")
     csv_header = next(csv_reader)
-    # this is a test to view the data
-    # print(csv_header)
-    # csv_firstrow = next(csv_reader)
-    # print(csv_firstrow)
-   
+
     # count rows and sum profits.  Note that the original data file included a last row that was null.  I deleted it.
-    first_row = next(csv_reader)
     row_count = 1
+    first_row = next(csv_reader)
     profit_total = int(first_row[1])
-    previous_row = int(first_row[1])
     total_changes = 0
+    previous_row = int(first_row[1])
     change_list = []
-    max_change_pl = first_row[1]
-    max_change_date = first_row[0]
-    min_change_pl = first_row[1]
-    min_change_date = first_row[0]
+    change_date = []
     
     for row in csv_reader:
         row_count +=  1
@@ -34,26 +27,27 @@ with open(bud_data_path, 'r', newline="") as bud_data:
         total_changes = change + total_changes
         previous_row = int(row[1])
         change_list.append(change)
+        change_date.append(row[0])
 
-
-        avg_change = total_changes/(row_count-1)
-        max_change = max(change_list)
-        min_change = min(change_list)
+    avg_change = total_changes/(row_count-1)
+    max_change = max(change_list)        
+    min_change = min(change_list)
         
-        # this assume that min and max cannot be the same date
+    max_index = change_list.index(max(change_list))
+    max_index_date = change_date[max_index] 
+    min_index = change_list.index(min(change_list))
+    min_index_date = change_date[min_index]
 
-    # print to terminal. Does not include date for greatest inc/dec at this time.
-    print({max_change_date})
-    print({min_change_date})
+    # print to terminal.
+
     print("Financial Analysis")
     print("---------------------------")
     print(f"Total Months: {row_count}")
     print("Total Profit/loss: $", format(profit_total, ",.2f"))
     print(f"Average Change: $", format(avg_change,",.2f"))
-    print(f"Greatest increase in profits: {max_change_date} $({max(change_list)})")
-    print(f"Greatest decrease in profits: {min_change_date} $({min(change_list)})")
+    print(f"Greatest increase in profits: {max_index_date} $({max(change_list)})")
+    print(f"Greatest decrease in profits: {min_index_date} $({min(change_list)})")
   
-
     # print to text file
         # first set file path
 
@@ -61,10 +55,11 @@ with open(bud_data_path, 'r', newline="") as bud_data:
         # file1 = open(final_file_path,"w")
 
 # with open("../PyBank/Analysis","a") as outputfile:
+    os.remove("Budget Analysis.txt")
     print("Financial Analysis", file=open("Budget Analysis.txt","a"))
     print("---------------------------",file=open("Budget Analysis.txt","a"))
     print(f"Total Months: {row_count}",file=open("Budget Analysis.txt","a"))
     print("Total Profit/loss: $", format(profit_total, ",.2f"),file=open("Budget Analysis.txt","a"))
-    print(f"Average Change: {total_changes/(row_count -1)}",file=open("Budget Analysis.txt","a"))
-    print(f"Greatest increase in profits: $({max(change_list)})",file=open("Budget Analysis.txt","a"))
-    print(f"Greatest decrease in profits: $({min(change_list)})",file=open("Budget Analysis.txt","a"))
+    print("Average Change: $", format(avg_change,",.2f"),file=open("Budget Analysis.txt","a"))
+    print(f"Greatest increase in profits: {max_index_date} $({max(change_list)})",file=open("Budget Analysis.txt","a"))
+    print(f"Greatest decrease in profits: {max_index_date} $({min(change_list)})",file=open("Budget Analysis.txt","a"))
