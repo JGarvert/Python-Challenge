fb39db62567dbec32f9f6294b4ec06b3b3a0b381
JGarvert <julie.garvert@adidas.com>
2021-01-08 19:47:12 -0800

    Pretty sure my code is screwed up to complete the entire homework assignment.

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,7 +14,6 @@ with open(bud_data_path, 'r', newline="") as bud_data:
     # print(csv_header)
     # csv_firstrow = next(csv_reader)
     # print(csv_firstrow)
-
    
     # count rows and sum profits.  Note that the original data file included a last row that was null.  I deleted it.
     first_row = next(csv_reader)
@@ -23,7 +22,9 @@ with open(bud_data_path, 'r', newline="") as bud_data:
     previous_row = int(first_row[1])
     total_changes = 0
     change_list = []
+    max_change_pl = first_row[1]
     max_change_date = first_row[0]
+    min_change_pl = first_row[1]
     min_change_date = first_row[0]
     
     for row in csv_reader:
@@ -33,42 +34,24 @@ with open(bud_data_path, 'r', newline="") as bud_data:
         total_changes = change + total_changes
         previous_row = int(row[1])
         change_list.append(change)
-        if row[1] == max_change:
-            max_change_date = row[0]
-        elif min_change == row[1]:
-            min_change_date = row[0]
-        else:
-            max_change_date = max_change_date
-            min_change_date = min_change_date
 
 
-    avg_change = total_changes/(row_count-1)
-    max_change = max(change_list)
-    min_change = min(change_list)
-
-    # # This assumes the min and max cannot be on the same date.
-    # for row in csv_reader:
-    #     if row[1] == max_change:
-    #         print({max_change})
-    #     else:
-    #         print("huh")
-    #     #     max_change_date = row[0]
-    #     # elif min_change == row[1]:
-    #     #     min_change_date = row[0]
-    #     # else:
-    #         # max_change_date = max_change_date
-            # min_change_date = min_change_date   
+        avg_change = total_changes/(row_count-1)
+        max_change = max(change_list)
+        min_change = min(change_list)
+        
+        # this assume that min and max cannot be the same date
 
     # print to terminal. Does not include date for greatest inc/dec at this time.
-    # print({max_change_date})
-    # print({min_change_date})
+    print({max_change_date})
+    print({min_change_date})
     print("Financial Analysis")
     print("---------------------------")
     print(f"Total Months: {row_count}")
-    # print("Total Profit/loss: $", format(profit_total, ",.2f"))
-    # print(f"Average Change: $", format(avg_change,",.2f"))
-    # print(f"Greatest increase in profits: {max_change_date} $({max(change_list)})")
-    # print(f"Greatest decrease in profits: {min_change_date} $({min(change_list)})")
+    print("Total Profit/loss: $", format(profit_total, ",.2f"))
+    print(f"Average Change: $", format(avg_change,",.2f"))
+    print(f"Greatest increase in profits: {max_change_date} $({max(change_list)})")
+    print(f"Greatest decrease in profits: {min_change_date} $({min(change_list)})")
   
 
     # print to text file
